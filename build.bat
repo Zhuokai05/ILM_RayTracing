a3365ffacfe304b2d0eee307cc5a36fa709b0723
ChichoRD <chichord2005@hotmail.com>
2026-04-21 16:08:18 +0200

    update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
+@echo off
+setlocal
+
 cd "%~dp0"
-clang++ -o out/main.exe .\src\main.cpp .\src\camera.cpp .\src\Film.cpp .\src\shape\sphere.cpp .\src\renderer.cpp .\src\scene.cpp
+
+@REM set compiler=clang++ 
+set compiler=g++ 
+%compiler% -o out/main.exe .\src\main.cpp .\src\camera.cpp .\src\Film.cpp .\src\shape\sphere.cpp .\src\renderer.cpp .\src\scene.cpp
 
 cd .\out
 .\main.exe
