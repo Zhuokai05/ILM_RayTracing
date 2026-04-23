6966e100addfce76feea4750f096d09e4652da62
Zhuokai Zhu <zhuzhu@ucm.es>
2026-04-23 16:00:49 +0200

    update build.bat

--- build.bat
+++ build.bat
@@ -10,6 +10,7 @@ set compiler=clang++
 mkdir out
 %compiler% ^
     -g ^
+    -std=c++17 ^
     -o out/main.exe ^
     .\src\main.cpp .\src\camera.cpp .\src\Film.cpp .\src\shape\sphere.cpp .\src\renderer.cpp .\src\scene.cpp .\src\light\light_directional.cpp ^
     -isystem vendor
