4f6ef671699caffad77679e339c741592bf32494
Chicho <chichord2005@hotmail.com>
2026-04-30 12:45:50 +0200

    add build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# compiler="${COMPILER:-clang++}"
+compiler="${COMPILER:-g++}"
+# compiler="${COMPILER:-cl}"
+
+mkdir -p out
+"$compiler" \
+    -g \
+    -std=c++17 \
+    -o out/main.exe \
+        ./src/main.cpp ./src/camera.cpp ./src/Film.cpp ./src/renderer.cpp ./src/scene.cpp ./src/material.cpp \
+        ./src/shape/sphere.cpp ./src/shape/quad.cpp \
+        ./src/light/light_directional.cpp ./src/light/light_ambient.cpp ./src/light/light_point.cpp \
+        ./src/texture/texture_checker.cpp ./src/texture/texture_constant.cpp ./src/texture/texture_uv.cpp \
+    -isystem vendor
+
+cd out
+./main.exe
